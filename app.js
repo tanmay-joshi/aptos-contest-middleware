09f96075af37a6b2b320bc96c8a60f41daf77aa4
tanmay-joshi <tanmayjoshi2205@gmail.com>
2024-07-07 17:40:13 +0530

    created a basic express app.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-//build an express server 
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');   
-}
-);
+    res.send('Welcome to Floc contests middleware1');
+});
+
+// start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
